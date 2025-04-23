781e565336ff735ab347a612099a7e8bc34b4035
Shenghong <me@zhoud2001.io>
2025-04-23 03:39:22 -0400

    Revert "the routing is the issue"
    This reverts commit 6d1152a3f1deee8f45c5d2c8d5b31013653e64b4.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router';
+import Home from './pages/Home';
+import About from './pages/About';
+
 function App() {
     return (
-        <h2>Hello!</h2>
+        <Router>
+            <nav>
+                <Link to="/">Home</Link> | <Link to="/about">About</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </Router>
     );
 }
 

da3e5e7ae733e9f53ec5e1d944f0263943fe4d13
Danny Zhou <me@zhoud2001.io>
2025-09-07 23:42:18 -0400

    hmmm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router';
+import { Routes, Route, Link } from 'react-router';
 import Home from './pages/Home';
 import About from './pages/About';
 
 function App() {
     return (
-        <Router>
+        <>
             <nav>
                 <Link to="/">Home!</Link> | <Link to="/about">About!</Link>
             </nav>
@@ -12,7 +12,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
             </Routes>
-        </Router>
+        </>
     );
 }
 

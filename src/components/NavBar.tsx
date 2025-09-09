b8b349d7a2e082a403df31983a8e911a8b89c4e8
Danny Zhou <me@zhoud2001.io>
2025-09-08 22:42:28 -0400

    hmmmm

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,34 @@
+import { useState } from 'react';
 import { Link } from 'react-router';
 
 import classes from '../css/nav.module.css';
 
 export default function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav className={classes.navbar}>
             <div className={classes.navbarLogo}>
                 <Link to="/">Veygo Rentals</Link>
             </div>
-            <ul className={classes.navbarLinks}>
-                <li><Link to="/pricing">Pricing</Link></li>
-                <li><Link to="/locations">Locations</Link></li>
-                <li><Link to="/about">About Us</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
-            <div className={classes.navbarAuth}>
-                <Link to="/login" className={classes.btn}>Sign In</Link>
-                <Link to="/signup" className={`${classes.btn} ${classes.btnPrimary}`}>Sign Up</Link>
+            <button
+                className={classes.hamburger}
+                onClick={() => setIsOpen(!isOpen)}
+                aria-label="Toggle menu"
+            >
+                ☰
+            </button>
+            <div className={`${classes.navContent} ${isOpen ? classes.show : ''}`}>
+                <ul className={classes.navbarLinks}>
+                    <li><Link to="/pricing">Pricing</Link></li>
+                    <li><Link to="/locations">Locations</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+                <div className={classes.navbarAuth}>
+                    <Link to="/login" className={classes.btn}>Sign In</Link>
+                    <Link to="/signup" className={`${classes.btn} ${classes.btnPrimary}`}>Sign Up</Link>
+                </div>
             </div>
         </nav>
     );

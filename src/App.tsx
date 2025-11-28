c338eeea0ad86039c16ddaa1ad26883aa1fd0c75
Shenghong <me@zhoud2001.io>
2025-11-28 11:12:14 -0500

    crisp

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Crisp } from "crisp-sdk-web";
+
 import { Routes, Route } from 'react-router';
 
 import NavBar from "./components/NavBar.tsx";
@@ -8,6 +10,9 @@ import About from './pages/About';
 import Policy from "./pages/Policy.tsx";
 
 function App() {
+
+    Crisp.configure("11d81aa1-c3e9-4295-a6ca-b207d63f37de");
+
     return (
         <>
             <NavBar />
